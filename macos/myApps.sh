3ca0d7bc69a7ad2a376ba13b70bf57d6b0e25f69
alexanderthoren <alexitoblogger@gmail.com>
2022-06-27 19:43:23 +0200

    -Removed AdGuard from myApps.

--- macos/myApps.sh
+++ macos/myApps.sh
@@ -2,12 +2,12 @@
 
 #Casks
 echo 'Installing casks'
-brew install 'adguard'
+#brew install 
 
 #Mac App Store
 echo 'Installing new apps'
 mas install 409183694 #Keynote
-mas install 441258766 #Magner
+mas install 441258766 #Magnet
 mas install 409203825 #Numbers
 mas install 409201541 #Pages
 mas install 497799835 #Xcode
