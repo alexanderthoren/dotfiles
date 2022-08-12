f9cdab2e80c6b171173eb97354b45dbaebf2eda7
alexanderthoren <alexitoblogger@gmail.com>
2022-08-13 00:11:24 +0200

    Added lua config for Macos and Windows.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,62 +1,56 @@
-local M = {}
+-- Indicate first time installation
+local packer_bootstrap = false
 
-function M.setup()
-	-- Indicate first time installation
-	local packer_bootstrap = false
+-- packer.nvim configuration
+local conf = {
+	display = {
+		open_fn = function()
+			return require('packer.util').float { border = 'rounded' }
+		end,
+	},
+}
 
-	-- packer.nvim configuration
-	local conf = {
-		display = {
-			open_fn = function()
-				return require("packer.util").float { border = "rounded" }
-			end,
-		},
-	}
-
-	-- Check if packer.nvim is installed
-	-- Run PackerCompile if there are changes in this file
-	local function packer_init()
-		local fn = vim.fn
-		local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-		if fn.empty(fn.glob(install_path)) > 0 then
-			packer_bootstrap = fn.system {
-				"git",
-				"clone",
-				"--depth",
-				"1",
-				"https://github.com/wbthomason/packer.nvim",
-				install_path,
-			}
-			vim.cmd [[packadd packer.nvim]]
-		end
-		vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
-	end
-
-	-- Plugins
-	local function plugins(use)
-
-		-- Packer
-		use { "wbthomason/packer.nvim" }
-
-		-- Colorscheme
-		use {
-			"sainnhe/sonokai",
-			config = function()
-				vim.cmd "colorscheme sonokai"
-			end,
+-- Check if packer.nvim is installed
+-- Run PackerCompile if there are changes in this file
+local function packer_init()
+	local fn = vim.fn
+	local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		packer_bootstrap = fn.system {
+			'git',
+			'clone',
+			'--depth',
+			'1',
+			'https://github.com/wbthomason/packer.nvim',
+			install_path,
 		}
-
-		if packer_bootstrap then
-			print "Restart Neovim required after installation!"
-			require("packer").sync()
-		end
+		vim.cmd [[packadd packer.nvim]]
 	end
-
-	packer_init()
-
-	local packer = require "packer"
-	packer.init(conf)
-	packer.startup(plugins)
+	vim.cmd 'autocmd BufWritePost plugins.lua source <afile> | PackerCompile'
 end
 
-return M
+-- Plugins
+local function plugins(use)
+
+	-- Packer
+	use { 'wbthomason/packer.nvim' }
+
+	-- Colorscheme
+	use {
+		'sainnhe/sonokai',
+		config = function()
+			vim.cmd 'colorscheme sonokai'
+		end,
+	}
+
+	if packer_bootstrap then
+		print 'Restart Neovim required after installation!'
+		require('packer').sync()
+	end
+end
+
+packer_init()
+
+local packer = require('packer')
+packer.init(conf)
+packer.startup(plugins)
