3a930d6242cb9d4927062146e6ca10cdc4730a21
alexanderthoren <alexitoblogger@gmail.com>
2022-08-19 20:20:57 +0200

    -Added Windows Terminal settings.json. -Removed old config in Windows install.sh

--- windows/install.sh
+++ windows/install.sh
@@ -2,23 +2,11 @@
 
 echo "Setting up WIN32"
 
-echo "Installing dependencies"
 # Install dependencies
+echo "Installing dependencies"
 choco install microsoft-windows-terminal
-choco install neovim
 choco install postman
 
-echo "Copying files"
-# Destination path
-NVIM_PATH=$HOME/AppData/Local/nvim
-
-# Remove old directories
-rm -rf $NVIM_PATH
-rm -rf $HOME/AppData/Local/nvim-data/site
-
-# Copy nvim directory to destination path
-cp -rf ./nvim $NVIM_PATH
-cp -rf .files/.gitconfig $HOME
-
+# Install WSL2
 echo "Instaling Windows Subsystem for Linux"
 wsl --install
