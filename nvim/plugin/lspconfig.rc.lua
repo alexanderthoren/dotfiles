3273c328294d0dcd5e70b5fd4cfb35ac81873613
alexanderthoren <alexitoblogger@gmail.com>
2022-09-14 20:17:14 +0200

    -Removed onAttach in lspconfig.rc.lua.

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -1,20 +1,8 @@
 local status, nvimLsp = pcall(require, 'lspconfig')
 if (not status) then return end
 
-local onAttach = function(client, bufnr)
-	-- formatting
-	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("Format", { clear = true }),
-			buffer = bufnr,
-			callback = function() vim.lsp.buf.formatting_seq_sync() end
-		})
-	end
-end
-
 -- Lua
 nvimLsp.sumneko_lua.setup {
-	on_attach = onAttach,
 	settings = {
 		Lua = {
 			diagnostics = {
