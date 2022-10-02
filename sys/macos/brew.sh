4d0ca0204824d022df0cc6ee043409fa5897a0a6
alexanderthoren <alexitoblogger@gmail.com>
2022-10-02 16:36:50 +0200

    -Brew file installation now using async await.

--- sys/macos/brew.sh
+++ sys/macos/brew.sh
@@ -15,32 +15,40 @@ update_upgrade_brew() {
 
 tap_dependencies() {
 	echo "Tap dependencies"
-	bt "homebrew/core" 
-	bt "romkatv/powerlevel10k"
-	bt "homebrew/cask"
+	(
+	bt "homebrew/core" &
+	bt "romkatv/powerlevel10k" &
+	bt "homebrew/cask" &
 	bt "homebrew/cask-fonts"
+	)
 	echo "Dependencies tap completed!"
 }
 
 install_casks() {
 	echo "Installing casks"
-	biq "alacritty" --no-quarantine
-	biq "proxyman"
+	(
+	biq "alacritty" --no-quarantine &
+	biq "proxyman" &
 	biq "stats"
+	)
+	wait
 	echo "Casks installation completed!"
 }
 
 install_binaries() {
 	echo "Installing binaries"
-	biq "coreutils"
-	biq "git"
-	biq "neovim"
-	biq "fzf"
-	biq "ripgrep"
-	biq "powerlevel10k"
-	biq "node"
-	biq "shellcheck"
+	(
+	biq "coreutils" &
+	biq "git" &
+	biq "neovim" &
+	biq "fzf" &
+	biq "ripgrep" &
+	biq "powerlevel10k" &
+	biq "node" &
+	biq "shellcheck" &
 	biq "TheZoraiz/ascii-image-converter/ascii-image-converter"
+	)
+	wait
 	echo "Binaries installation completed!"
 }
 
@@ -48,6 +56,9 @@ echo "-> Brew installation"
 verify_install_brew
 update_upgrade_brew
 tap_dependencies
-install_casks
+(
+install_casks &
 install_binaries
+)
+wait
 echo "<- Brew installation completed!"
