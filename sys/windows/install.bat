005c4708dcb87d9138b4920a96722db49846bfc9
alexanderthoren <alexitoblogger@gmail.com>
2022-10-06 19:43:40 +0200

    -Windows install.bat now copies alacritty.yml into the correct path.

--- sys/windows/install.bat
+++ sys/windows/install.bat
@@ -1,2 +1,5 @@
 @ECHO OFF
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+::@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+mkdir C:%HOMEPATH%\AppData\Roaming\alacritty\
+copy .\alacritty\alacritty.yml C:%HOMEPATH%\AppData\Roaming\alacritty\
