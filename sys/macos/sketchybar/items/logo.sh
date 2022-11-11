83edfe00f9f9e793eeddf5785559d38d5cd4fa3f
alexanderthoren <alexitoblogger@gmail.com>
2022-11-11 23:21:16 +0100

    -Updated logo item label to current user name.

--- sys/macos/sketchybar/items/logo.sh
+++ sys/macos/sketchybar/items/logo.sh
@@ -3,7 +3,7 @@
 sketchybar --add item logo left \
 		   --set logo icon= \
 					  icon.padding_right=2 \
-					  label="alexander"	\
+					  label="$USER"	\
 					  label.padding_left=2 \
 					  background.color=0x44ffffff \
 					  background.drawing=off \
