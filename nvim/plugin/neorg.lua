e33f6b52188d081e8942fcc058cd69558719a9d7
alexanderthoren <alexitoblogger@gmail.com>
2022-12-04 16:29:46 +0100

    -Corrected neorg setup.

--- nvim/plugin/neorg.lua
+++ nvim/plugin/neorg.lua
@@ -5,7 +5,15 @@ neorg.setup {
 	load = {
 		["core.defaults"] = {},
 		["core.norg.concealer"] = {},
-		["core.norg.completion"] = {},
-		["core.presenter"] = {}
+		["core.norg.completion"] = {
+			config = {
+				engine = 'nvim-cmp'
+			}
+		},
+		["core.presenter"] = {
+			config = {
+				zen_mode = 'zen-mode'
+			}
+		}
 	}
 }
