add3609c2f4ad798fc4de060f8f6f6930d28b0de
alexanderthoren <alexitoblogger@gmail.com>
2023-02-26 00:33:32 +0100

    -Added maps for telescope with grep.

--- sys/shared/.config/nvim/plugin/telescope.lua
+++ sys/shared/.config/nvim/plugin/telescope.lua
@@ -1,6 +1,22 @@
 local status, telescope = pcall(require, 'telescope')
 if (not status) then return end
 
-telescope.setup {}
+telescope.setup {
+	pickers = {
+		find_files = {
+			hidden = true
+		},
+		grep_string = {
+			additional_args = function()
+				return {"--hidden"}
+			end
+		},
+		live_grep = {
+			additional_args = function()
+				return {"--hidden"}
+			end
+		}
+	}
+}
 
 telescope.load_extension('fzf')
