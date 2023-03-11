fbd318cf3dafa21e67f6365f7aa6d4aaae922a3f
alexanderthoren <alexitoblogger@gmail.com>
2023-03-11 13:14:39 +0100

    -Telescope now ignores .git files.

--- sys/shared/.config/nvim/plugin/telescope.lua
+++ sys/shared/.config/nvim/plugin/telescope.lua
@@ -4,14 +4,26 @@ if (not status) then return end
 telescope.setup {
 	pickers = {
 		find_files = {
+			file_ignore_patterns = {
+				".git/"
+			},
 			hidden = true
 		},
 		grep_string = {
+			file_ignore_patterns = {
+				".git/"
+			},
 			additional_args = function()
 				return {"--hidden"}
 			end
 		},
+			file_ignore_patterns = {
+				".git/"
+			},
 		live_grep = {
+			file_ignore_patterns = {
+				".git/"
+			},
 			additional_args = function()
 				return {"--hidden"}
 			end
