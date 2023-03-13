6aafde7e2b9646e66fc22b4b3dfb566d839f04a6
alexanderthoren <alexitoblogger@gmail.com>
2023-03-13 20:06:10 +0100

    -Added not lazy loading for some plugins.

--- sys/shared/.config/nvim/lua/plugins.lua
+++ sys/shared/.config/nvim/lua/plugins.lua
@@ -18,10 +18,12 @@ return {
 	'kyazdani42/nvim-web-devicons',
 	'kyazdani42/nvim-tree.lua',
 	'nvim-lua/plenary.nvim',
-	'nvim-telescope/telescope.nvim',
 	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-		run = 'make'
+		'nvim-telescope/telescope.nvim',
+		dependencies = {
+			'nvim-telescope/telescope-fzf-native.nvim',
+			build = 'make'
+		}
 	},
 	{
 		'nvim-treesitter/nvim-treesitter',
@@ -29,37 +31,56 @@ return {
 			require('nvim-treesitter.install').update({ with_sync = true })
 		end,
 	},
- 'samodostal/image.nvim',
- 'folke/trouble.nvim',
- 'williamboman/mason.nvim',
- 'williamboman/mason-lspconfig.nvim',
- 'neovim/nvim-lspconfig',
- 'hrsh7th/cmp-buffer',
- 'hrsh7th/cmp-nvim-lsp',
- 'hrsh7th/nvim-cmp',
- 'onsails/lspkind.nvim',
- 'L3MON4D3/LuaSnip',
- 'saadparwaiz1/cmp_luasnip',
- 'hrsh7th/cmp-path',
- 'hrsh7th/cmp-cmdline',
- 'simrat39/symbols-outline.nvim',
- 'lewis6991/gitsigns.nvim',
- 'petertriho/nvim-scrollbar',
- 'folke/twilight.nvim',
- 'folke/zen-mode.nvim',
- 'nvim-treesitter/nvim-treesitter-context',
- 'rmagatti/auto-session',
- 'sindrets/winshift.nvim',
- 'NvChad/nvim-colorizer.lua',
- 'phaazon/hop.nvim',
- {
+	'samodostal/image.nvim',
+	'folke/trouble.nvim',
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
+	'neovim/nvim-lspconfig',
+	'hrsh7th/nvim-cmp',
+	'onsails/lspkind.nvim',
+	'L3MON4D3/LuaSnip',
+	'simrat39/symbols-outline.nvim',
+	'lewis6991/gitsigns.nvim',
+	'petertriho/nvim-scrollbar',
+	'folke/twilight.nvim',
+	'folke/zen-mode.nvim',
+	'rmagatti/auto-session',
+	'sindrets/winshift.nvim',
+	'NvChad/nvim-colorizer.lua',
+	'phaazon/hop.nvim',
+	'windwp/nvim-autopairs',
+	'kylechui/nvim-surround',
+	'pwntester/octo.nvim',
+	'folke/todo-comments.nvim',
+	'numToStr/Comment.nvim',
+	'ray-x/lsp_signature.nvim',
+	{
 		'romgrk/barbar.nvim',
+		lazy = false,
 		wants = 'nvim-web-devicons'
 	},
- 'windwp/nvim-autopairs',
- 'kylechui/nvim-surround',
- 'pwntester/octo.nvim',
- 'folke/todo-comments.nvim',
- 'numToStr/Comment.nvim',
- 'ray-x/lsp_signature.nvim',
+	{
+		'nvim-treesitter/nvim-treesitter-context',
+		lazy = false,
+	},
+	{
+		'hrsh7th/cmp-buffer',
+		lazy = false,
+	},
+	{
+		'hrsh7th/cmp-nvim-lsp',
+		lazy = false,
+	},
+	{
+		'saadparwaiz1/cmp_luasnip',
+		lazy = false,
+	},
+	{
+		'hrsh7th/cmp-path',
+		lazy = false,
+	},
+	{
+		'hrsh7th/cmp-cmdline',
+		lazy = false,
+	}
 }
