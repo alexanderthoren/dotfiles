7beeee111e7065227903430d5c1e7ddc33781fc5
alexanderthoren <alexitoblogger@gmail.com>
2023-03-18 12:03:46 +0100

    -Corrected archlinux after update.

--- sys/arch/.home/.screenlayout/base_layout.sh
+++ sys/arch/.home/.screenlayout/base_layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --off --output DP-1 --primary --mode 1920x1080 --pos 0x487 --rotate normal --rate 143.98 --output DP-2 --off --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate right --output DP-3 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate right --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --rate 143.98 --pos 0x480 --rotate normal --output DP-5 --off
