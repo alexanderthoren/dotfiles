e08d2b7742d2a3a9fa8df4c75e2228528ea59599
alexanderthoren <alexitoblogger@gmail.com>
2023-10-19 12:13:44 +0200

    Corrected nvimtree mappings.

--- sys/shared/.config/nvim/plugin/nvimtree.lua
+++ sys/shared/.config/nvim/plugin/nvimtree.lua
@@ -7,12 +7,17 @@ end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
+local function my_on_attach(bufnr)
+	local api = require('nvim-tree.api')
+	api.config.mappings.default_on_attach(bufnr)
+	vim.keymap.del('n', 's', { buffer = bufnr })
+end
+
 tree.setup {
 	disable_netrw = true,
 	hijack_netrw = true,
 	sort_by = 'case_sensitive',
-	remove_keymaps = {'s'},
-	on_attach = 'disabled',
+	on_attach = my_on_attach,
 	view = {
 		side = "left",
 		width = 30,
