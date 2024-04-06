a8320f74b137e404e005ae287203b0c09e914de5
alexanderthoren <alexitoblogger@gmail.com>
2024-04-06 22:43:06 +0200

    Improved full_installation script

--- full_installation.sh
+++ full_installation.sh
@@ -41,7 +41,22 @@ brew install font-source-code-pro
 # Install apps
 brew install --no-quarantine alacritty
 
+# Install system plugins
+brew tap koekeishiya/formulae
+brew tap FelixKratz/formulae
+brew install yabai
+brew install skhd
+brew install borders
+brew install sketchybar
+
 # Install terminal plugins
 brew install tmux
 brew install starship
 brew install nvim
+brew install bat
+brew install fzf
+brew install ripgrep
+brew install pre-commit
+brew install xcodes
+brew install sourcery
+brew install peripheryapp/periphery/periphery
