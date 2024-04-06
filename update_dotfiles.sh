72b901efeed865871c33cf79c5e023cdf3bfb3a2
alexanderthoren <alexitoblogger@gmail.com>
2024-04-06 22:21:48 +0200

    Added update_dotfiles script.

---
+++ update_dotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# This file only adds the latest changes of your system to your dotfiles
+
+# Copy macos home files
+cp ~/.zshrc sys/macos/.home
+cp ~/.zprofile sys/macos/.home
+
+# Copy macos ~/.config files
+cp ~/.config/alacritty sys/macos/.config
+
+# Copy shared home files
+cp ~/.tmux.conf sys/shared/.home
+
+# Copy shared ~/.config files
+#cp sys/shared/.config/starship.toml ~/.config
+cp -r ~/.config/nvim sys/shared/.config
