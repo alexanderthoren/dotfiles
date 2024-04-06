7e47ad15fa71f9cd960f560cbdb4061d62871770
alexanderthoren <alexitoblogger@gmail.com>
2024-04-06 23:10:55 +0200

    Added zip apps installation to full_installation script

--- full_installation.sh
+++ full_installation.sh
@@ -63,3 +63,11 @@ brew install xcodes
 brew install sourcery
 brew install peripheryapp/periphery/periphery
 brew install node
+
+# Install zip apps
+cp -r sys/macos/apps/Vimac.zip /Applications/
+tar -xf /Applications/Vimac.zip -C /Applications/
+rm /Applications/Vimac.zip
+cp -r sys/macos/apps/SourceryPro.zip /Applications/
+tar -xf /Applications/SourceryPro.zip -C /Applications/
+rm /Applications/SourceryPro.zip
