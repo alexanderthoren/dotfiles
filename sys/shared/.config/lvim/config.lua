806aac01aa9835e61e8f3e2f94edeb4ba367fa68
alexanderthoren <alexitoblogger@gmail.com>
2024-04-20 12:53:20 +0200

    Added lvim theme config.

--- sys/shared/.config/lvim/config.lua
+++ sys/shared/.config/lvim/config.lua
@@ -2,3 +2,17 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
+  {
+    "sainnhe/gruvbox-material",
+    name = "gruvbox",
+    priority = 1000
+  },
+}
+
+lvim.colorscheme = "gruvbox-material"
