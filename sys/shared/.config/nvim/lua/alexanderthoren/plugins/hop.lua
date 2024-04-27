36e84b0f4b413206ca9001de0aeb88ce3e23534d
alexanderthoren <alexitoblogger@gmail.com>
2024-04-28 00:42:05 +0200

    Added hop plugin.

---
+++ sys/shared/.config/nvim/lua/alexanderthoren/plugins/hop.lua
@@ -0,0 +1,14 @@
+return {
+  "phaazon/hop.nvim",
+  event = "BufRead",
+  config = function()
+    local hop = require("hop")
+
+    hop.setup({})
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>fh", ":HopChar2<cr>", { desc = "Hop with 2 chars" })
+    keymap.set("n", "<leader>fH", ":HopWord<cr>", { desc = "Hop to word" })
+  end,
+}
