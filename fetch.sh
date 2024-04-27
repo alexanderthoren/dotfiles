a5d100640bd3b7aaf91eebd911ae9d682e3fd779
alexanderthoren <alexitoblogger@gmail.com>
2024-04-27 16:09:56 +0200

    Fetch now can be called from everywhere.

--- fetch.sh
+++ fetch.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
+dotfiles_dir="$(dirname "$0")"
+
 # Copy macos home files
-cp ~/.zshrc sys/macos/.home
-cp ~/.zprofile sys/macos/.home
-cp ~/.aerospace.toml sys/macos/.home
-cp ~/.gitignore sys/macos/.home
+cp ~/.zshrc "$dotfiles_dir/sys/macos/.home"
+cp ~/.zprofile "$dotfiles_dir/sys/macos/.home"
+cp ~/.aerospace.toml "$dotfiles_dir/sys/macos/.home"
+cp ~/.gitignore "$dotfiles_dir/sys/macos/.home"
 
 # Copy macos ~/.config files
-rm -r sys/macos/.config/alacritty
-cp -r ~/.config/alacritty sys/macos/.config
-rm -r sys/macos/.config/sketchybar
-cp -r ~/.config/sketchybar sys/macos/.config
-rm -r sys/macos/.config/borders
-cp -r ~/.config/borders sys/macos/.config
+rm -r "$dotfiles_dir/sys/macos/.config/alacritty"
+cp -r ~/.config/alacritty "$dotfiles_dir/sys/macos/.config"
+rm -r "$dotfiles_dir/sys/macos/.config/sketchybar"
+cp -r ~/.config/sketchybar "$dotfiles_dir/sys/macos/.config"
+rm -r "$dotfiles_dir/sys/macos/.config/borders"
+cp -r ~/.config/borders "$dotfiles_dir/sys/macos/.config"
 
 # Copy shared home files
-cp ~/.tmux.conf sys/shared/.home
+cp ~/.tmux.conf "$dotfiles_dir/sys/shared/.home"
 
 # Copy shared ~/.config files
-cp  ~/.config/starship.toml sys/shared/.config
-rm -r sys/shared/.config/lvim
-cp -r ~/.config/lvim sys/shared/.config
-rm -r sys/shared/.config/nvim
-cp -r ~/.config/nvim sys/shared/.config
+cp  ~/.config/starship.toml "$dotfiles_dir/sys/shared/.config"
+rm -r "$dotfiles_dir/sys/shared/.config/lvim"
+cp -r ~/.config/lvim "$dotfiles_dir/sys/shared/.config"
+rm -r "$dotfiles_dir/sys/shared/.config/nvim"
+cp -r ~/.config/nvim "$dotfiles_dir/sys/shared/.config"
