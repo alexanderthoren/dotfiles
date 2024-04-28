07cb4b5b88da3f2f9cdf5c1e20694fa80379ad21
alexanderthoren <alexitoblogger@gmail.com>
2024-04-28 23:38:01 +0200

    Updated find references in lspconfig.

--- sys/shared/.config/nvim/lua/alexanderthoren/plugins/lsp/lspconfig.lua
+++ sys/shared/.config/nvim/lua/alexanderthoren/plugins/lsp/lspconfig.lua
@@ -19,7 +19,7 @@ return {
         local opts = { buffer = ev.buf, silent = true }
 
         opts.desc = "Show LSP references"
-        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+        keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
 
         opts.desc = "Go to declaration"
         keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
