e58183f292ec7a6485f4f94ae1bda5d3f4911305
alexanderthoren <alexitoblogger@gmail.com>
2024-04-30 20:24:57 +0200

    Corrected ubuntu update.sh.

--- sys/ubuntu/update.sh
+++ sys/ubuntu/update.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
+dotfiles_dir="$(dirname "$0")"
+
 # Copy home files
-cp ./sys/shared/.home/.zshrc ~
-cp ./sys/shared/.home/.gitignore ~
+cp "$dotfiles_dir/../shared/.home/.zshrc" ~
+cp "$dotfiles_dir/../shared/.home/.gitignore" ~
 
 # Copy ~/.config files
 alacrity_path=~/.config/alacritty
 if [ -d "$alacrity_path" ]; then rm -rf "$alacrity_path"; fi
-cp -r ./sys/ubuntu/.config/alacritty ~/.config
+cp -r "$dotfiles_dir/.config/alacritty" ~/.config
 
 # Copy shared home files
-cp ./sys/shared/.home/.tmux.conf ~
+cp "$dotfiles_dir/../shared/.home/.tmux.conf" ~
 
 # Copy shared ~/.config files
-cp ./sys/shared/.config/starship.toml ~/.config
+cp "$dotfiles_dir/../shared/.config/starship.toml" ~/.config
 nvim_cache_path=~/.cache/nvim
 if [ -d "$nvim_cache_path" ]; then rm -rf $nvim_cache_path; fi
 nvim_local_share_path=~/.local/share/nvim/
 if [ -d "$nvim_local_share_path" ]; then rm -rf $nvim_local_share_path; fi
 nvim_path=~/.config/nvim
 if [ -d "$nvim_path" ]; then rm -rf $nvim_path; fi
-cp -r ./sys/shared/.config/nvim ~/.config
+cp -r "$dotfiles_dir/../shared/.config/nvim" ~/.config
