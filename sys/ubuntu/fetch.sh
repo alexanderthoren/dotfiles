ca8db58965640e3252b137ad1e2d70a20b79b854
alexanderthoren <alexitoblogger@gmail.com>
2024-05-03 00:57:32 +0200

    Added fixed screen resolution.

--- sys/ubuntu/fetch.sh
+++ sys/ubuntu/fetch.sh
@@ -4,13 +4,16 @@ dotfiles_dir="$(dirname "$0")"
 
 # Copy ubuntu home files
 cp -r ~/.xsessionrc "$dotfiles_dir/.home"
+cp -r ~/.screenlayout "$dotfiles_dir/.home"
 
 # Copy ubuntu ~/.config files
 rm -r "$dotfiles_dir/.config/alacritty"
 cp -r ~/.config/alacritty "$dotfiles_dir/.config"
-#rm -r "$dotfiles_dir/.config/bspwm"
+rm -r "$dotfiles_dir/.config/bspwm"
 cp -r ~/.config/bspwm "$dotfiles_dir/.config"
+rm -r "$dotfiles_dir/.config/sxhkd"
 cp -r ~/.config/sxhkd "$dotfiles_dir/.config"
+rm -r "$dotfiles_dir/.config/rofi"
 cp -r ~/.config/rofi "$dotfiles_dir/.config"
 
 # Copy shared home files
