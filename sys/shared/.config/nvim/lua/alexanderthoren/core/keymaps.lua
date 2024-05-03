c0d4c8e0b008ca11816822158c13e8af05932b77
alexanderthoren <alexitoblogger@gmail.com>
2024-05-03 13:33:57 +0200

    Improved swapping of splits.

--- sys/shared/.config/nvim/lua/alexanderthoren/core/keymaps.lua
+++ sys/shared/.config/nvim/lua/alexanderthoren/core/keymaps.lua
@@ -20,14 +20,23 @@ keymap.set("n", "ya", "gg<S-v>Gy", { desc = "Yank all file" })
 
 keymap.set("n", "da", "gg<S-v>Gd", { desc = "Delete all file" })
 
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Navigate down and center" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Navigate up and center" })
+
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>ss", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split windows equal size" })
 keymap.set("n", "<leader>sc", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set("n", "<leader>sx", "<C-w><C-x>", { desc = "Exchange current splits" })
+
 keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to window at the left" })
 keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to the window below" })
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to the window above" })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the window at the right" })
+keymap.set("n", "<leader><C-h>", "<C-w>H", { desc = "Swap to window at the left" })
+keymap.set("n", "<leader><C-J>", "<C-w>J", { desc = "Swap to the window below" })
+keymap.set("n", "<leader><C-K>", "<C-w>K", { desc = "Swap to the window above" })
+keymap.set("n", "<leader><C-L>", "<C-w>L", { desc = "Swap to the window at the right" })
 
 keymap.set("n", "<leader>tt", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current tab" })
