35fd5a347107746a1ad654c428e928cad37e7556
alexanderthoren <alexitoblogger@gmail.com>
2024-11-23 00:03:31 +0100

    feat(neotest): add neotest plugin configuration
    - Updated lazy-lock.json to include new plugin versions.
    - Added neotest.lua configuration file for Neotest plugin.
    - Configured key mappings for running and managing tests.
    - Included dependencies for Neotest plugin.

---
+++ sys/shared/.config/nvim/lua/alexanderthoren/plugins/neotest.lua
@@ -0,0 +1,68 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-python",
+  },
+  config = function()
+    local neotest = require("neotest")
+    local neotest_python = require("neotest-python")
+
+    ---@diagnostic disable-next-line: missing-fields
+    neotest.setup({
+      adapters = {
+        neotest_python({
+          dap = { justMyCode = false },
+        }),
+      },
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set(
+      "n",
+      "<leader>nr",
+      ":Neotest run<CR>",
+      { desc = "Run tests in file" }
+    )
+    keymap.set(
+      "n",
+      "<leader>ns",
+      ":Neotest stop<CR>",
+      { desc = "Stop the running tests" }
+    )
+    keymap.set(
+      "n",
+      "<leader>nj.",
+      ":Neotest jump next<CR>",
+      { desc = "Jump to the next test" }
+    )
+    keymap.set(
+      "n",
+      "<leader>nj,",
+      ":Neotest jump prev<CR>",
+      { desc = "Jump to the previous test" }
+    )
+    keymap.set(
+      "n",
+      "<leader>no",
+      ":Neotest output<CR>",
+      { desc = "Show tests results in floating view" }
+    )
+    keymap.set(
+      "n",
+      "<leader>np",
+      ":Neotest output-panel<CR>",
+      { desc = "Show tests results in split view" }
+    )
+    keymap.set(
+      "n",
+      "<leader>nn",
+      ":Neotest summary<CR>",
+      { desc = "Show shummary" }
+    )
+  end,
+}
