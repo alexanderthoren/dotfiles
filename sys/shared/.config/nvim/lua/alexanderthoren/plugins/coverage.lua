1906e9e43b3849031ed3abe543804d8722503f4f
alexanderthoren <alexitoblogger@gmail.com>
2024-11-23 01:20:34 +0100

    feat(nvim): add nvim-coverage plugin
    - Added nvim-coverage plugin to track code coverage
    - Configured plugin with key mappings for loading, clearing, toggling,
      showing summary, and navigating coverage signs
    - Updated lazy-lock.json to include nvim-coverage plugin

---
+++ sys/shared/.config/nvim/lua/alexanderthoren/plugins/coverage.lua
@@ -0,0 +1,63 @@
+return {
+  "andythigpen/nvim-coverage",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    auto_reload = true,
+    load_coverage_cb = function(ftype)
+      vim.notify("Loaded " .. ftype .. " coverage")
+    end,
+    lang = {
+      python = {
+        coverage_file = vim.fn.getcwd() .. "/.coverage",
+        coverage_command = vim.fn.getcwd()
+          .. "/.venv/bin/coverage json -q -o -",
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>ncl",
+      function()
+        require("coverage").load(true)
+      end,
+      desc = "load coverage report",
+    },
+    {
+      "<leader>ncc",
+      function()
+        require("coverage").clear()
+      end,
+      desc = "clear coverage report",
+    },
+    {
+      "<leader>nct",
+      function()
+        require("coverage").toggle()
+      end,
+      desc = "toggle coverage signs",
+    },
+    {
+      "<leader>ncs",
+      function()
+        require("coverage").summary()
+      end,
+      desc = "show coverage summary",
+    },
+    {
+      "<leader>nc.",
+      function()
+        require("coverage").jump_next("uncovered")
+      end,
+      desc = "jump to next uncovered sign",
+    },
+    {
+      "<leader>nc,",
+      function()
+        require("coverage").jump_prev("uncovered")
+      end,
+      desc = "jump to prev uncovered sign",
+    },
+  },
+}
