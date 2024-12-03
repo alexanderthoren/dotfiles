7fc473f7505be18dd551827bb21c94bed8333272
alexanderthoren <alexitoblogger@gmail.com>
2024-12-03 22:04:45 +0100

    feat(nvim): add line-number-change-mode plugin
    - Updated lazy-lock.json with new plugin versions.
    - Added line-number-change-mode.nvim plugin configuration.
    - Configured color settings for different modes.

---
+++ sys/shared/.config/nvim/lua/alexanderthoren/plugins/ui/line-number.lua
@@ -0,0 +1,45 @@
+return {
+  "sethen/line-number-change-mode.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local line_number = require("line-number-change-mode")
+
+    local colors = {
+      blue = "#7daea3",
+      green = "#a9b665",
+      violet = "#d3869b",
+      red = "#ea6962",
+      fg = "#1b1b1b",
+    }
+
+    line_number.setup({
+      mode = {
+        i = {
+          bg = colors.green,
+          fg = colors.fg,
+          bold = true,
+        },
+        n = {
+          bg = colors.blue,
+          fg = colors.fg,
+          bold = true,
+        },
+        R = {
+          bg = colors.red,
+          fg = colors.fg,
+          bold = true,
+        },
+        v = {
+          bg = colors.violet,
+          fg = colors.fg,
+          bold = true,
+        },
+        V = {
+          bg = colors.violet,
+          fg = colors.fg,
+          bold = true,
+        },
+      },
+    })
+  end,
+}
